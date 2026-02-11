f0ba41265db48409cb0354c2c0adee536fc46a7a
dmanjarrezprado-sudo <dmanjarrezprado@mail.weber.edu>
2026-02-10 21:04:06 -0700

    actualizacion 1

--- modulo5/ejercicio1_boolean/codigo1.py
+++ modulo5/ejercicio1_boolean/codigo1.py
@@ -7,27 +7,42 @@ es_finde = False
 
 # TODO 1: Usa una expresión booleana con "and"
 # Por ejemplo: ¿Puede salir hoy solo si tiene 18 años o más Y si tiene permiso?
+
+print("Puede salir hoy solo si tiene 18 años o más Y si tiene permiso")
 edad = input("cuantos años tienes")
 edad = int(edad)
-print("¿Puede salir hoy solo si tiene 18 años o más Y si tiene permiso?")
-puede_salir = (edad >= 18) and tiene_permiso
+if (edad >= 18) and tiene_permiso:
+    print("puede salir")
+else:
+    print("no puede salir")
 
-print("puede salir")
 
 
 
 
 # TODO 2: Usa una expresión booleana con "or" para permitir salir si es fin de semana o tiene permiso
+es_finde = input("es fin de semana? (si/no)")
+if es_finde or tiene_permiso:
+    print("andar en las calles, puedes salir")
+else:
+    print("no puedes salir, solo puedes estar en casa")
 
 
 
 # TODO 3: Usa una expresión booleana con "not" para negar una condición
 # Por ejemplo: De ninguna manera tiene permiso
+print("no tiene permiso")
+
 
 
 
 # TODO 5: Escribe tu propia condición con and, or o not e imprimelo a la pantalla.
-# Ejemplo: ¿Puede conducir si tiene 18 o más y tiene permiso? u cualquier otra idea. 
+# Ejemplo: ¿Puede conducir si tiene 18 o más y tiene permiso? u cualquier otra idea.
+# para andar con permisos debe tener 21 an
+if (edad >= 21) and tiene_permiso:
+        print("puede conducir")
+
+
 
 
 
--- modulo5/ejercicio2_cuadrado/codigo2.py
+++ modulo5/ejercicio2_cuadrado/codigo2.py
@@ -3,36 +3,40 @@ import math
 # Crear una función para verificar si un número es un cuadrado perfecto
 # Completar las 3 tareas especificadas con TODO
 
-def es_cuadrado_perfecto(n):
-    # TODO 1: Si el número es negativo, no puede ser un cuadrado perfecto. 
-    # Asi que utiliza if para ver si n es menos que 0,
-    # si lo es que devuelva un Falso para nuestro Booleano
-   
+#def es_cuadrado_perfecto(n):
+# TODO 1: Si el número es negativo, no puede ser un cuadrado perfecto. 
+# Asi que utiliza if para ver si n es menos que 0,
+# si lo es que devuelva un Falso para nuestro Booleano
+n = int(input("Ingrese un número: "))
 
-    # Calcular la raíz cuadrada entera del número
-    raiz_entera = int(n ** 0.5)  
+if n < 0:
+    print(f" {n} < 0")
 
-    # Verificar si la raíz al cuadrado es igual al número original
-    #return raiz_entera * raiz_entera == n
-    return math.isqrt(n) ** 2 == n
+# Calcular la raíz cuadrada entera del número
+raiz_entera = int(n ** 0.5)  
+
+# Verificar si la raíz al cuadrado es igual al número original
+#return raiz_entera * raiz_entera == n
+math.isqrt(n) ** 2 == n
 
 # Entrada del usuario
-num_usuario = int(input("¿Qué número te gustaría revisar si es cuadrado perfecto? "))
+num_usuario = n
 
 # Mostrar el resultado
-if es_cuadrado_perfecto(num_usuario):
+if math.isqrt(num_usuario) ** 2 == num_usuario:
     print(f" El número {num_usuario} es un cuadrado perfecto.")
 else:
     #TODO 2: Dar un mensaje al usuario si NO es cuadrado perfecto. 
+    print(f" El número {num_usuario} NO es un cuadrado perfecto.")
     
 
 
 # TODO 3: Puedes encontrar el error en el siguiente codigo? Quita los simbolos # para probar el codigo linea por linea.
 
-# Este codigo prueba si es cuadrado perfecto de una vez con una lista de valores
-#print("\n Pruebas automáticas con varios valores:")
-#test_valores = [0, 1, 4, 9, 16, 25, 26, 27, 100, 101, -1, -4]
-#for num in test_values:
-#    resultado = es_cuadrado_perfecto(num)
-#    estado = "✅" if resultado else "❌"
-#    print(f"{estado} {num} {'es' if resultado else 'NO es'} un cuadrado perfecto.")
+#Este codigo prueba si es cuadrado perfecto de una vez con una lista de valores
+print("\n Pruebas automáticas con varios valores:")
+test_valores = [0, 1, 4, 9, 16, 25, 26, 27, 100, 101, -1, -4]
+for num in test_valores:
+   resultado = es_cuadrado_perfecto(num)
+   estado = "✅" if resultado else "❌"
+   print(f"{estado} {num} {'es' if resultado else 'NO es'} un cuadrado perfecto.")
